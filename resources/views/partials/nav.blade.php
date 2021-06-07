26ca4d049a5fa36a5dcfcc3f31b64a2334edbc7e
HiddeVanLeeuwen <tijdvoorgamezz@gmail.com>
2021-06-07 11:12:48 +0200

    blade afgemaakt

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,30 +1,46 @@
-<nav class="fixed-top d-flex navbar navbar-expand-lg navbar-light justify-content-between" style="background-color: rgba(255, 162, 0, 0.8); )">
-    <a class="navbar-brand" href="#">Imkervereniging Oegstgeest</a>
+<nav class="fixed-top navbar navbar-expand-lg navbar-light justify-content-between" style="background-color: rgba(255, 162, 0, 0.8); )">
+    <div>
+    <a class="navbar-brand col-6" href="#">Imkervereniging Oegstgeest</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
+    </div>
 
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
+    <div class="collapse navbar-collapse d-flex justify-content-end col-6"  id="navbarSupportedContent">
+            <ul style="margin-left: 3vw;" class="navbar-nav mr-auto ">
                 <li class="nav-item active">
                     <a class="nav-link" href="#">Home</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Imkervereniging</a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Artikelen</a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Nieuws Archief</a>
                 </li>
                 <li>
                     <div class="dropdown show">
-                    <a class="nav-link dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Cursussen
-                    </a>
+                        <a class="nav-link dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Cursussen
+                        </a>
 
-                    <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-                        <a class="dropdown-item" href="#">cursus1</a>
-                        <a class="dropdown-item" href="#">cursus2</a>
-                        <a class="dropdown-item" href="#">cursus3</a>
+                        <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                            <a class="dropdown-item" href="#">Bijscholing</a>
+                            <a class="dropdown-item" href="#">Basiscursus</a>
+                            <a class="dropdown-item" href="#">Cursusblog</a>
+                        </div>
                     </div>
-        </div>
                 </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Voor Imkers</a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Contact</a>
+                </li>
+
+
             </ul>
         </div>
 </nav>

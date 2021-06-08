9a594b80974fd3737ba129fe9c25f646570390e5
HiddeVanLeeuwen <tijdvoorgamezz@gmail.com>
2021-06-08 09:08:10 +0200

    nav aangepast
    Ik heb de navigatie verwijderd en opnieuw opgebouwd omdat er een glitch in zat en die is nu opgelost

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,44 +1,43 @@
-<nav class="fixed-top navbar navbar-expand-lg navbar-light d-flex justify-content-between col-12" style="background-color: rgba(255, 162, 0, 0.8); )">
-    <div class="col-3 ">
-    <a class="navbar-brand" href="#">Imkervereniging Oegstgeest</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
+<nav class="fixed-top navbar navbar-expand-lg navbar-light col-12 d-flex justify-content-between" style="background-color: rgba(255, 162, 0, 0.8); )">
+    <a class="navbar-brand col-3" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+<div class="col-9 d-flex justify-content-end align-items-end">
+    <div class="collapse navbar-collapse col-lg-8 col-md-12 col-sm-12" id="navbarSupportedContent">
+        <ul class="navbar-nav">
+                            <li class="nav-item active">
+                                <a class="nav-link" href="#">Home</a>
+                            </li>
+                            <li class="nav-item active">
+                                <a class="nav-link" href="#">Imkervereniging</a>
+                            </li>
+                            <li class="nav-item active">
+                                <a class="nav-link" href="#">Artikelen</a>
+                            </li>
+                            <li class="nav-item active">
+                                <a class="nav-link" href="#">Nieuws Archief</a>
+                            </li>
+                            <li>
+                                <div class="dropdown">
+                                    <a class="nav-link dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        Cursussen
+                                    </a>
+
+                                    <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                                        <a class="dropdown-item" href="#">Bijscholing</a>
+                                        <a class="dropdown-item" href="#">Basiscursus</a>
+                                        <a class="dropdown-item" href="#">Cursusblog</a>
+                                    </div>
+                                </div>
+                            </li>
+                            <li class="nav-item active">
+                                <a class="nav-link" href="#">Voor Imkers</a>
+                            </li>
+                            <li class="nav-item active">
+                                <a class="nav-link" href="#">Contact</a>
+                            </li>
+                        </ul>
     </div>
-
-    <div class="collapse navbar-collapse d-flex align-items-end justify-content-end col-9"  id="navbarSupportedContent">
-            <ul class="navbar-nav">
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">Home</a>
-                </li>
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">Imkervereniging</a>
-                </li>
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">Artikelen</a>
-                </li>
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">Nieuws Archief</a>
-                </li>
-                <li>
-                    <div class="dropdown show">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Cursussen
-                        </a>
-
-                        <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-                            <a class="dropdown-item" href="#">Bijscholing</a>
-                            <a class="dropdown-item" href="#">Basiscursus</a>
-                            <a class="dropdown-item" href="#">Cursusblog</a>
-                        </div>
-                    </div>
-                </li>
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">Voor Imkers</a>
-                </li>
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">Contact</a>
-                </li>
-            </ul>
-        </div>
+</div>
 </nav>

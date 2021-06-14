2c110fc037b30412c6a35bcc5a15318898bf7e71
wesley Schellingerhoudt <6006266@mborijnland.nl>
2021-06-14 11:32:17 +0200

    create form

---
+++ resources/views/admin.blade.php
@@ -0,0 +1,12 @@
+@extends('layout')
+{{--@include('nav')--}}
+@section('title')
+    HalloTest
+@endsection
+
+@section('body')
+    <div class="d-flex justify-content-center align-items-center col-12 col-md-12 col-lg-12">
+        <h1>Home</h1>
+    </div>
+    @include('artikel.create')
+@endsection

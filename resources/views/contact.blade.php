4097b99f2b949f5777c2efcb87fce27929561273
HiddeVanLeeuwen <tijdvoorgamezz@gmail.com>
2021-06-14 12:55:52 +0200

    Create contact.blade.php

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,17 @@
+@extends('layout')
+
+@section('title')
+    Home - Imkervereniging
+@endsection
+
+@section('webTitle')
+    Home
+@endsection
+
+@section('content')
+
+@endsection
+
+@section('afbeelding')
+    <img src="{{ asset('images/placeholder.png') }}" alt="afbeelding" class="rounded img-fluid" style="height: 30vh">
+@endsection
